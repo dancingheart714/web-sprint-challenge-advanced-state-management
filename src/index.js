f9f258bc397b870f89ca145658beaca6ea9298ee
Leah King <dancingheart714@yahoo.com>
2021-04-16 10:19:42 -0400

    store created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
 
 import "./index.css";
 import App from "./App";
+
+const store = createStore
+(reducer,
+applyMiddleware(logger)
+);
 
 const { worker } = require('./mocks/browser');
 worker.start();
@@ -10,11 +18,13 @@ worker.start();
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
-    rootElement
+    <Provider store ={store}>
+    <App />,
+    </Provider>
+    rootElement,
 );
 
 //Task List:
-//1. Add in all necessary components and libary methods.
-//2. Create a store that includes thunk and logger middleware support.
-//3. Wrap the App component in a react-redux Provider element.
+//=>1. Add in all necessary components and libary methods.
+//=>2. Create a store that includes thunk and logger middleware support.
+//=>3. Wrap the App component in a react-redux Provider element.
